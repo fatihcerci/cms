4788212754b6a14ddc5b79d8f977b964f351ab11
F161 <F161@FSBLGOAB-4Z65.fibasigorta.pvt>
2021-02-25 17:26:09 +0300

    notif popup style
    Signed-off-by: F161 <F161@FSBLGOAB-4Z65.fibasigorta.pvt>

--- panel/application/views/includes/notifications_popup.php
+++ panel/application/views/includes/notifications_popup.php
@@ -1,3 +1,20 @@
+<style>
+    #notificationsPopup 
+        .modal-title {
+            margin: 0;
+            line-height: 0.7;
+        }
+    #notificationsPopup    
+        .panel {
+            margin-bottom: 0px;
+        }
+    #notificationsPopup 
+        .media-group-item {
+            padding: 0px 0px;
+        }
+    }
+</style>
+
 <?php 
 
 $user_all_notifications = get_all_notifications();
@@ -5,41 +22,37 @@ $user_all_notifications = get_all_notifications();
 if($user_all_notifications) { ?>
     
     <div class="modal fade" id="notificationsPopup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog" role="document">
         <!--Content-->
         <div class="modal-content">
           <!--Header-->
 <!--           <div class="modal-header"> -->
-          	<div class="panel panel-danger">
+          	<div class="panel panel-primary">
     			<div class="panel-heading" style="padding: 12px; !important">
-            	<h4 class="modal-title w-100 text-center" id="myModalLabel">Bildirimler</h4>
+            	<h5 class="modal-title w-100 text-center" id="myModalLabel">Bildirimler</h5>
             	</div>
             </div>
 <!--           </div> -->
           <!--Body-->
-          <div class="modal-body">
-            <?php foreach($user_all_notifications as $key => $item) { ?>
-            	<a href="<?php echo base_url('notifications/view_all_notifications') ?>" class="media-group-item" id="media<?php echo $key ?>">
-                	<div class="media">
-                   		<div class="media-left">
-                      		<div class="avatar avatar-md avatar-circle" style="margin-top: 6px;">
-                        		<img src="<?php echo base_url("assets"); ?>/assets/images/218.jpg" alt="">
-                      		</div>
-                    	</div>
-                    	<div class="media-body">
-                      		<h5 class="media-heading"><?php echo $item['full_name'] ?></h5>
-                      		<small class="media-meta"><?php echo $item['description'] ?></small>
-                    	</div>
-                    	<small class="text-muted fz-sm pull-right" style="font-size:11px !important"><?php echo $item['gecenSure'] ?></small>
-                  	</div>
-                </a><!-- .media-group-item -->
-            <?php } ?>
-          </div>
-          <!--Footer-->
-          <div class="modal-footer text-center">
-          
-          	
-          </div>
+            <div class="modal-body">
+                <?php foreach($user_all_notifications as $key => $item) { ?>
+                    <a href="<?php echo base_url('notifications/view_all_notifications') ?>" class="media-group-item" id="media<?php echo $key ?>">
+                    	<div class="media">
+                       		<div class="media-left">
+                          		<div class="avatar avatar-md avatar-circle" style="margin-top: 6px;">
+                            		<img src="<?php echo base_url("assets"); ?>/assets/images/218.jpg" alt="">
+                          		</div>
+                        	</div>
+                        	<div class="media-body">
+                          		<h5 class="media-heading"><?php echo $item['full_name'] ?></h5>
+                          		<small class="media-meta"><?php echo $item['description'] ?></small>
+                          		<small class="text-muted fz-sm pull-right" style="font-size:11px !important"><?php echo $item['gecenSure'] ?></small>
+                        	</div>
+                        	
+                      	</div>
+                    </a><!-- .media-group-item -->
+                <?php } ?>
+            </div>
         </div>
         <!--/.Content-->
         </div>
